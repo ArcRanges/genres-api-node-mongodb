028813385b3faaa779688f53c867eca03eaab3f4
Lovely <arc.ranges@live.ca>
2021-03-24 23:55:13 -0700

    update routes

--- startup/routes.js
+++ startup/routes.js
@@ -13,10 +13,10 @@ module.exports = function (app) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  if (process.env.NODE_ENV === "development") {
-    app.use(morgan("tiny"));
-    winston.info("morgan enabled...");
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   app.use(morgan("tiny"));
+  //   winston.info("morgan enabled...");
+  // }
 
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
